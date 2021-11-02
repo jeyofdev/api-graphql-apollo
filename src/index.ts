2a0fd52b4f17e80dda1c1a6a8ca11443c5a4162c
jeyofdev <jgregoire.pro@gmail.com>
2021-11-02 10:44:58 +0100

    feat : init api with apollo-server and graphql

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,55 @@
-console.log('ok');
+import { ApolloServer, gql } from 'apollo-server';
+import dotenv from 'dotenv';
 
-const testForLinter = 'test';
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = gql`
+  type Query {
+    movies: [Movie]
+  }
+  type Movie {
+    id: Int
+    title: String
+    director: String
+    year: Int
+    rating: Int
+    duration: Int
+    type: String
+  }
+`;
+
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars : a new hope',
+    director: 'George Lucas',
+    year: '1977',
+    rating: 8.6,
+    duration: 121,
+    type: 'Sci-Fi',
+  },
+  {
+    id: 2,
+    title: 'The Godfather',
+    director: 'Francis Ford Coppola',
+    year: '1972',
+    rating: 9.2,
+    duration: 175,
+    type: 'Drama',
+  },
+];
+
+const resolvers = {
+  Query: {
+    movies: () => movies,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen(PORT).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
